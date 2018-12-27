2d9ea369ffb41dae9d800cec31a9288d6d12ad6e
ranimufid <ranimufid@gmail.com>
2018-12-27 17:46:31 +0100

    set default engine type

--- terraform/aws-rds/variables.tf
+++ terraform/aws-rds/variables.tf
@@ -1,16 +1,16 @@
 variable "identifier" {
-  # default = "mydb-rds"
   description = "Identifier for your DB, example: guestbookrdsinstance"
+  default     = "guestbookrdsinstance"
 }
 
 variable "storage" {
-  default     = "5"
   description = "Storage size in GB"
+  default     = "5"
 }
 
 variable "engine" {
-  default     = "mysql"
   description = "Engine type, example: mysql or postgres"
+  default     = "mysql"
 }
 
 variable "engine_version" {
@@ -23,8 +23,8 @@ variable "engine_version" {
 }
 
 variable "instance_class" {
-  default     = "db.t2.micro"
   description = "DB Size. Examples: db.t2.micro (1vCPU 1GiB), db.t2.small (1vCPU 2GiB), db.t2.medium (2vCPU 4GiB), db.t2.large (2vCPU 8GiB)"
+  default     = "db.t2.micro"
 }
 
 variable "db_name" {
