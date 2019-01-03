928bf86d2a0bca6ab380457ff627993d5b5ca003
ranimufid <ranimufid@gmail.com>
2019-01-03 17:47:18 +0100

    update rds instance name

--- terraform/aws-rds/variables.tf
+++ terraform/aws-rds/variables.tf
@@ -1,6 +1,6 @@
 variable "identifier" {
   description = "Identifier for your DB, example: guestbookrdsinstance"
-  default     = "guestbookrdsinstance"
+  default     = "guestbookrdsinstance-new"
 }
 
 variable "storage" {
