d45c4b14127dc278c002af726340f0e3a6c11c32
ranimufid <ranimufid@gmail.com>
2019-01-04 10:10:25 +0100

    fix formatting

--- terraform/aws-rds/main.tf
+++ terraform/aws-rds/main.tf
@@ -1,27 +1,28 @@
 provider "aws" {
   region = "eu-central-1"
 }
-resource "aws_s3_bucket" "terraform_state_storage_s3_jenkins_pipeline" {
-    bucket = "terraform-remote-state-storage-s3-jenkins-pipeline"
 
-    versioning {
-      enabled = true
-    }
- 
-    lifecycle {
-      prevent_destroy = true
-    }
- 
-    tags {
-      Name = "S3 Remote Terraform State Store"
-    }      
+resource "aws_s3_bucket" "terraform_state_storage_s3_jenkins_pipeline" {
+  bucket = "terraform-remote-state-storage-s3-jenkins-pipeline"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags {
+    Name = "S3 Remote Terraform State Store"
+  }
 }
 
 terraform {
- backend "s3" {
- encrypt = true
- bucket = "terraform-remote-state-storage-s3-jenkins-pipeline"
- region = "eu-central-1"
- key = "dokcer-ecs"
- }
+  backend "s3" {
+    encrypt = true
+    bucket  = "terraform-remote-state-storage-s3-jenkins-pipeline"
+    region  = "eu-central-1"
+    key     = "dokcer-ecs"
+  }
 }
